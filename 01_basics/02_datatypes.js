cb815a0ede41f2b893f017db562a3c6781c4110c
Jaya Singh <108207406+JayaSingh26@users.noreply.github.com>
2024-12-29 06:48:14 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all js code as newer version
+
+
+// alert(3+3 ) // we are using node js not browser
+
+
+// code readability should be high
+console.log(" hey Jaya")
+
+console.log(3 + 3)
+
+let name = " Jaya Singh"
+let age = 23
+let isLoggedIn = false
+let state = null 
+
+
+//number => 2 to power 53
+//bigint 
+//string => ".. ", '..'
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned
+//symbol => unique 
+
+
+//object 
+
+console.log(typeof 23); // to know the type of datatype
+
+console.log(typeof null);// it is an object
+
+
+console.log(typeof undefined);// it is undefined
