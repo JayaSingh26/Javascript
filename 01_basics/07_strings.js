812308a2766cf4a05d5c428b6f7475650eaf0f5c
Jaya Singh <108207406+JayaSingh26@users.noreply.github.com>
2024-12-31 08:12:18 +0000

    string opeartions

--- 01_basics/07_strings.js
+++ 01_basics/07_strings.js
@@ -5,4 +5,10 @@ const reposcount = 50
 // console.log(name + reposcount + " value");
 console.log(`Hello my name is ${name} and my repo is ${reposcount}`);
 
+const gameName = new String("JayaThakur")
+//by using this method we can get value directly in key value pair and can be accessed
+console.log(gameName);
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+//(.) is used to use other methods with string
 
