e9957988b02a637919cb11c6b0d496209df113b8
Jaya Singh <108207406+JayaSingh26@users.noreply.github.com>
2024-12-31 04:19:12 +0000

    summary of  datatypes

--- 01_basics/05_datatypes_summary.js
+++ 01_basics/05_datatypes_summary.js
@@ -10,11 +10,11 @@ const screVALUE = 100.3
 const isLoggedIn = false
 const outsidetemp= null
 let userEmail;
-console.log(typeof userEmail)
+console.log(typeof userEmail);
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
-console.log(id===anotherId);
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id===anotherId);
 
 //const bigNumber = 13452345456n
 
@@ -25,16 +25,27 @@ console.log(id===anotherId);
 
 //Array, Objects, Fuctions
 
-const heros = ["Shaktimaan", "Spiderman"]
+// const heros = ["Shaktimaan", "Spiderman"]
 
-let myobj = {
-    name:"jaya",
-    age:22
-}
+// let myobj = {
+//     name:"jaya",
+//     age:22,
+// }
 
-const myFunction =  function(){
-console.log("hello World")
-}
+// const myFunction =  function(){
+// console.log("hello World")
+// }
 
 
+
+//*************************************************** */
+//Stack (Primitive), Heap (Non-Primitive)
+
+let anothername = "Chhotu"
+
+let collegename = anothername
+anothername = "JayaSingh"
+
+console.log(anothername);
+console.log(collegename);
 
