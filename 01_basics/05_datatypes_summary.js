07d55d1982301d52ccae81ceb9b84d025c340da3
Jaya Singh <108207406+JayaSingh26@users.noreply.github.com>
2024-12-31 04:11:00 +0000

    datatypessummary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,40 @@
+//  Primitive 
+
+// 7 types: String , number, boolean, null, undefined, symbol, BigInt
+
+
+
+const score =100
+const screVALUE = 100.3
+
+const isLoggedIn = false
+const outsidetemp= null
+let userEmail;
+console.log(typeof userEmail)
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id===anotherId);
+
+//const bigNumber = 13452345456n
+
+
+
+
+//Refreence type or Non Primitve
+
+//Array, Objects, Fuctions
+
+const heros = ["Shaktimaan", "Spiderman"]
+
+let myobj = {
+    name:"jaya",
+    age:22
+}
+
+const myFunction =  function(){
+console.log("hello World")
+}
+
+
+
