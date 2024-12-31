238818d02c8fe27767e9be8f945effee9b0e2941
Jaya Singh <108207406+JayaSingh26@users.noreply.github.com>
2024-12-31 03:50:17 +0000

    comparisonOfdatatypes

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,4 +1,27 @@
-console.log(2>1)
-console.log(2<1)
-console.log(2>=1)
-console.log(2<=1)
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2>=1)
+// console.log(2<=1)
+// console.log(2==1)
+// console.log(2!=1);
+
+// console.log("2">1)
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null<0);
+// console.log(null>=0);// true 
+
+
+// console.log(undefined ==0);
+// console.log(undefined <0);
+// console.log(undefined >0);
+
+
+//avoid the above type of comparison in the block 
+
+//=== it strictly check the datatype and value as well 
+console.log("2"===2)
+
+
